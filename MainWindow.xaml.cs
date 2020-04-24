55487d278228f16402827ecb0110edb0641cec9e
VajdaBela <vajdabela01@gmail.com>
2020-04-24 14:42:34 +0200

    minor layout changes, cities are read from the internet

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+using Newtonsoft.Json;
 
 namespace HappyWeather
 {
@@ -20,9 +15,20 @@ namespace HappyWeather
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<City> Allcities;
+        public ObservableCollection<City> filteredCities { get; set; }
+        public ObservableCollection<City> selectedCities { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+            filteredCities = new ObservableCollection<City>();
+            filteredCities.Add(new City { name = "Novi Sad"});
+            filteredCities.Add(new City { name = "Beograd" });
+
+            selectedCities = new ObservableCollection<City>();
+
             List<VremePodatak> vremena = new List<VremePodatak>();
             vremena.Add(new VremePodatak { VremeMerenja = new DateTime(), Temperatura = 10, VazdusniPritisak = 11, Vidljivost = 12, Vlaznost = 13 });
             vremena.Add(new VremePodatak { VremeMerenja = new DateTime(), Temperatura = 10, VazdusniPritisak = 11, Vidljivost = 12, Vlaznost = 13 });
@@ -34,7 +40,30 @@ namespace HappyWeather
             vremena.Add(new VremePodatak { VremeMerenja = new DateTime(), Temperatura = 10, VazdusniPritisak = 11, Vidljivost = 12, Vlaznost = 13 });
             vremena.Add(new VremePodatak { VremeMerenja = new DateTime(), Temperatura = 10, VazdusniPritisak = 11, Vidljivost = 12, Vlaznost = 13 });
             vremena.Add(new VremePodatak { VremeMerenja = new DateTime(), Temperatura = 10, VazdusniPritisak = 11, Vidljivost = 12, Vlaznost = 13 });
+
+            ReadCities();
+            Allcities.Sort(City.CompareCity);
+            updateFilteredListBox();
             MainDataGrid.ItemsSource = vremena;
+        }
+
+        public void updateFilteredListBox()
+        {
+            filteredCities.Clear();
+            string searchString = SearchTextBox.Text;
+            int numberAdded = 0;
+            foreach(City city in Allcities)
+            {
+                if(city.name.Contains(searchString))
+                {
+                    filteredCities.Add(city);
+                    numberAdded += 1;
+                }
+                if(numberAdded >= 100)
+                {
+                    break;
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -50,7 +79,40 @@ namespace HappyWeather
             ChoicesStackPanel.Visibility = Visibility.Hidden;
             MainDataGrid.Visibility = Visibility.Visible;
         }
+        private void ReadCities()
+        {
+            WebClient wb = new WebClient();
+            wb.DownloadFile("http://bulk.openweathermap.org/sample/city.list.json.gz", Path.GetTempPath() + "city.list.json.gz");
+            using (FileStream file = new FileStream(Path.GetTempPath() + "city.list.json.gz", FileMode.Open))
+            using (GZipStream decompresor = new GZipStream(file, CompressionMode.Decompress))
+            using (StreamReader jsonText = new StreamReader(decompresor, Encoding.UTF8))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                Allcities = (List<City>)serializer.Deserialize(jsonText, typeof(List<City>));
+            }
+        }
+
+        private void SearchTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            updateFilteredListBox();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            City city = (City)FilteredListBox.SelectedItem;
+            selectedCities.Add(city);
+            filteredCities.Remove(city);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            City city = (City)SelectedListBox.SelectedItem;
+            selectedCities.Remove(city);
+            updateFilteredListBox();
+        }
     }
+
+
 
     public class VremePodatak
     {
@@ -59,5 +121,30 @@ namespace HappyWeather
         public double VazdusniPritisak { get; set; }
         public double Vidljivost { get; set; }
         public double Vlaznost { get; set; }
+    }
+
+    public class City
+    {
+        public string id;
+        public string name;
+        public string state;
+        public string country;
+        public Coordinate coord;
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        public static int CompareCity(City x, City y)
+        {
+            return x.name.ToLower().CompareTo(y.name.ToLower());
+        }
+    }
+
+    public class Coordinate
+    {
+        public string lon;
+        public string lat;
     }
 }
